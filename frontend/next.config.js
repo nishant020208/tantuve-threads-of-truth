6808737a8b4e90683626ccf7ff3f004d8576ba84
nishant020208 <nishant020208@gmail.com>
2026-08-21 17:52:54 +0530

    Add Next.js frontend config and package files

---
+++ frontend/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "gateway.pinata.cloud" },
+      { protocol: "https", hostname: "ipfs.io" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
