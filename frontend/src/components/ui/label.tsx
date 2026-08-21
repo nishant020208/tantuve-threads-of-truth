7ee39a8f7cc44feb02e52f0da7b84d47a767126c
nishant020208 <nishant020208@gmail.com>
2026-08-21 18:07:27 +0530

    Remove @radix-ui/react-label dependency from Label component

--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -1,17 +1,16 @@
 import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
 import { cn } from "@/lib/utils";
 
 const Label = React.forwardRef<
-  React.ComponentRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+  HTMLLabelElement,
+  React.LabelHTMLAttributes<HTMLLabelElement>
 >(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
+  <label
     ref={ref}
     className={cn("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
     {...props}
   />
 ));
-Label.displayName = LabelPrimitive.Root.displayName;
+Label.displayName = "Label";
 
 export { Label };
