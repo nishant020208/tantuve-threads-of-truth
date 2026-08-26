9dcd5d3f3e0d5a10682f322b8ebb52a55ab6416e
nishant020208 <nishant020208@gmail.com>
2026-08-26 20:47:23 +0530

    Add validation states to Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, type, ...props }, ref) => {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  isInvalid?: boolean;
+  isValid?: boolean;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, isInvalid = false, isValid = false, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          isInvalid && "border-destructive/50 bg-destructive/5",
+          isValid && "border-teal/50 bg-teal/5",
           className
         )}
         ref={ref}
